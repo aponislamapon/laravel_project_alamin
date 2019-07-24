c4abf7025c550fc1dae5e746aab06ee471b3ea79
Apon Islam AL-AMIN <48382430+aponislamapon@users.noreply.github.com>
2019-07-24 00:05:15 -0600

    cart

--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -144,8 +144,9 @@
                                     <img src="{{ $details['photo'] }}" />
                                 </div>
                                 <div class="col-lg-8 col-sm-8 col-8 cart-detail-product">
-                                    <p>{{ $details['name'] }}</p>
-                                    <span class="price text-info"> ${{ $details['price'] }}</span> <span class="count"> Quantity:{{ $details['quantity'] }}</span>
+                                    <p style="font-weight: bold; font-size: 16px;">{{ $details['name'] }}</p>
+                                    <p><span class="price text-info"> ${{ $details['price'] }}</span> <span class="count"> Quantity:{{ $details['quantity'] }}</span></p>
+                                    
                                 </div>
                             </div>
                         @endforeach
