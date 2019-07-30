afffda363fa1bc63ea564d9e4e934511bf8173d6
aponislamapon <aponislamapon@gmail.com>
2019-07-29 19:20:20 -0700

    login

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V2</title>
+	<title>Login V4</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -30,47 +30,67 @@
 <body>
 	
 	<div class="limiter">
-		<div class="container-login100" >
-			<div class="wrap-login100" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 15.19);">
-				<form class="login100-form validate-form" action="{{url('/admin-dashboard')}}" method="post">
+		<div class="container-login100" style="background-image: url('{{URL::to('asset_login/images/bg-01.jpg')}}');">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+				<form class="login100-form validate-form" method="post" action="{{url('/admin-dashboard')}}">
 					{{csrf_field()}}
-					<span class="login100-form-title p-b-26">
-						Admin Login
-					</span>
-					<span class="login100-form-title p-b-48">
-						
+					<span class="login100-form-title p-b-49">
+						Login
 					</span>
 
-					<div class="wrap-input100 validate-input" >
-						<input class="input100" type="text" name="email">
-						<span class="focus-input100" data-placeholder="Email"></span>
+					<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+						<span class="label-input100">email</span>
+						<input class="input100" type="text" name="email" placeholder="Type your username">
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input">
-						<span class="btn-show-pass">
-							<i class="zmdi zmdi-eye"></i>
-						</span>
-						<input class="input100" type="password" name="password">
-						<span class="focus-input100" data-placeholder="Password"></span>
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="password" placeholder="Type your password">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
 					</div>
-
+					
+					<div class="text-right p-t-8 p-b-31">
+						<a href="#">
+							Forgot password?
+						</a>
+					</div>
+					
 					<div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
-
-
 							<button type="submit" class="login100-form-btn">
 								Login
 							</button>
 						</div>
 					</div>
 
-					<div class="text-center p-t-115">
-						<span class="txt1">
-							Don’t have an account?
+					<div class="txt1 text-center p-t-54 p-b-20">
+						<span>
+							Or Sign Up Using
+						</span>
+					</div>
+
+					<div class="flex-c-m">
+						<a href="#" class="login100-social-item bg1">
+							<i class="fa fa-facebook"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg2">
+							<i class="fa fa-twitter"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg3">
+							<i class="fa fa-google"></i>
+						</a>
+					</div>
+
+					<div class="flex-col-c p-t-155">
+						<span class="txt1 p-b-17">
+							Or Sign Up Using
 						</span>
 
-						<a class="txt2" href="#">
+						<a href="#" class="txt2">
 							Sign Up
 						</a>
 					</div>
